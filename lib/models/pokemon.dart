47bca73cb0aa05819f0fa92904a4d16a0bd329fe
Vadim <vadimtomin21@yandex.by>
2023-04-24 10:22:55 +0200

    add types
    add types in model

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -4,21 +4,27 @@ class Pokemon {
   final String imageUrl;
   final int weight;
   final int height;
+  final List<dynamic> types;
 
   Pokemon(
       {required this.name,
       required this.id,
       required this.imageUrl,
       required this.weight,
-      required this.height});
+      required this.height,
+      required this.types});
 
   factory Pokemon.fromJson(Map<String, dynamic> json) {
+    final types = json['types']
+        .map<String>((type) => type['type']['name'].toString())
+        .toList();
     return Pokemon(
       name: json['name'],
       id: json['id'],
       imageUrl: json['sprites']['front_default'],
       weight: json['weight'],
       height: json['height'],
+      types: types,
     );
   }
 }
