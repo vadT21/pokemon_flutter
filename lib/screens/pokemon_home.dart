4617dc9e9bfabe773dd2c023517e7dbe314a38bf
Vadim <vadimtomin21@yandex.by>
2023-04-25 00:46:31 +0200

    refactoring code
    refactoring code

--- lib/screens/pokemon_home.dart
+++ lib/screens/pokemon_home.dart
@@ -4,6 +4,7 @@ import 'package:pokemon_flutter/models/pokemon.dart';
 import 'package:pokemon_flutter/screens/pokemon_detail.dart';
 import 'package:pokemon_flutter/services/pokemon_db.dart';
 import 'package:pokemon_flutter/services/pokemon_api_service.dart';
+import 'package:pokemon_flutter/widgets/pokemon_card.dart';
 
 class PokemonHomePage extends StatefulWidget {
   @override
@@ -23,18 +24,22 @@ class _PokemonHomePageState extends State<PokemonHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pokemon application'),
+        title: const Text('Pokemon application'),
       ),
       body: _pokemonList.isEmpty
+          //если нету данных то показывается загрузка
           ? const Center(
               child: CircularProgressIndicator(),
             )
+          //построение grid для отображения элементов
           : GridView.count(
               crossAxisCount: 2,
               crossAxisSpacing: 10,
               mainAxisSpacing: 10,
               children: _pokemonList.map((pokemon) {
+                // отвечает за нажатие на элемент
                 return GestureDetector(
+                  // переход на дет. информацию при клике
                   onTap: () {
                     Navigator.push(
                       context,
@@ -45,88 +50,12 @@ class _PokemonHomePageState extends State<PokemonHomePage> {
                       ),
                     );
                   },
-                  child: Card(
-                    child: Container(
-                      padding: const EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/images/background.jpg'),
-                          fit: BoxFit.cover,
-                          colorFilter: ColorFilter.mode(
-                            Color.fromARGB(255, 255, 255, 255).withOpacity(0.8),
-                            BlendMode.srcATop,
-                          ),
-                        ),
-                      ),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text('#${pokemon.id}'),
-                              Container(
-                                width: 96,
-                                height: 96,
-                                decoration: BoxDecoration(
-                                  color: Colors.green,
-                                  borderRadius: BorderRadius.circular(50),
-                                ),
-                                child: Image.network(
-                                  pokemon.imageUrl,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            pokemon.name.toUpperCase(),
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                              shadows: [
-                                Shadow(
-                                  blurRadius: 5,
-                                  color: Colors.grey,
-                                  offset: Offset(1, 2),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Row(
-                            children: [
-                              Wrap(
-                                spacing: 8.0,
-                                children: pokemon.types
-                                    .map(
-                                      (type) => Container(
-                                        padding: const EdgeInsets.symmetric(
-                                            vertical: 4.0, horizontal: 10.0),
-                                        decoration: BoxDecoration(
-                                          color: Colors.green,
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                        ),
-                                        child: Text(
-                                          '$type',
-                                          style: const TextStyle(
-                                              fontSize: 14,
-                                              color: Colors.white),
-                                        ),
-                                      ),
-                                    )
-                                    .toList(),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                  // отрисовка элемента
+                  child: PokemonCardWidget(pokemon: pokemon),
                 );
               }).toList(),
             ),
+      //кнопка для добавления элементов
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           fetchData();
@@ -136,20 +65,25 @@ class _PokemonHomePageState extends State<PokemonHomePage> {
     );
   }
 
+// метод получения данных
   void fetchData() async {
+    // проверяем доступ к интернету
     final connectivityResult = await Connectivity().checkConnectivity();
+    //получаем количество элементов из базы данных
     final countItems = await DatabaseHelper.countItems();
-    final currentPage = (countItems / 10).toInt();
+    // получаем число с которого нужно загружать новые элементы
+    final currentPage = (countItems ~/ 10).toInt();
     if (connectivityResult == ConnectivityResult.none) {
       // нет подключения к интернету
     } else {
-      // есть подключение к интернету
+      // есть подключение к интернету, делаем запрос к API
       final apiData = await fetchPokemonList(currentPage);
       // Сохраняем данные в базу данных
       for (final pokemon in apiData) {
         await DatabaseHelper.insertPokemon(pokemon);
       }
     }
+    //получаем список из базы данных
     final pokemonList = await DatabaseHelper.getPokemonList();
     setState(() {
       _pokemonList = pokemonList;
