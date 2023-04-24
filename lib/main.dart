c0b044245a0b0b27a875371fe2a4cd961cf271c9
Vadim <vadimtomin21@yandex.by>
2023-04-24 21:53:29 +0200

    refactoring code
    refactoring code

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:pokemon_flutter/models/pokemon.dart';
 import 'package:pokemon_flutter/screens/pokemon_detail.dart';
 import 'package:pokemon_flutter/screens/pokemon_home.dart';
-import 'package:pokemon_flutter/services/pokemon_api_service.dart';
-import 'package:pokemon_flutter/services/pokemon_db.dart';
 
 void main() async {
-  // Инициализируем базу данных
   WidgetsFlutterBinding.ensureInitialized();
-  // Загружаем данные из API
-
-  // Проверка статуса подключения к интернету
-  final connectivityResult = await Connectivity().checkConnectivity();
-  if (connectivityResult == ConnectivityResult.none) {
-    // нет подключения к интернету
-  } else {
-    // есть подключение к интернету
-    final apiData = await fetchPokemonList();
-    // Сохраняем данные в базу данных
-    for (final pokemon in apiData) {
-      await DatabaseHelper.insertPokemon(pokemon);
-    }
-  }
-  // Запускаем приложение
-
   runApp(PokemonApp());
 }
 
@@ -34,7 +14,7 @@ class PokemonApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pokémon App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       initialRoute: '/',
       routes: {
