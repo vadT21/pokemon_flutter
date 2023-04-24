2cd055bc84ce42ed6a0ad317317b3a61d2a701a2
Vadim <vadimtomin21@yandex.by>
2023-04-24 19:12:04 +0200

    rebuilding the display model
    rebuilding the display model

--- lib/screens/pokemon_detail.dart
+++ lib/screens/pokemon_detail.dart
@@ -14,58 +14,109 @@ class PokemonDetailScreen extends StatelessWidget {
         title: Text(pokemon.name),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SizedBox(height: 16),
-          Image.network(
-            pokemon.imageUrl,
-            height: 200,
-            width: 200,
-            fit: BoxFit.cover,
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '#${pokemon.id}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      '${pokemon.name}',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+              // второй столбец
+              Expanded(
+                child: Row(
+                  children: [
+                    Wrap(
+                      spacing: 8.0, // расстояние между элементами списка
+                      children: pokemon.types
+                          .map(
+                            (type) => Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 4.0, horizontal: 8.0),
+                              decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: Text(
+                                '$type',
+                                style: const TextStyle(fontSize: 14),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          SizedBox(height: 16),
-          Text(
-            'Name: ${pokemon.name}',
-            style: TextStyle(fontSize: 24),
+          Row(
+            children: [
+              Expanded(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    Row(
+                      children: [Text('hp'), Text('${pokemon.stats[0]}')],
+                    ),
+                    Row(
+                      children: [Text('attack'), Text('${pokemon.stats[1]}')],
+                    ),
+                    Row(
+                      children: [Text('defense'), Text('${pokemon.stats[2]}')],
+                    ),
+                    Row(
+                      children: [Text('speed'), Text('${pokemon.stats[5]}')],
+                    ),
+                  ],
+                ),
+              ),
+              // второй столбец
+              Expanded(
+                child: Image.network(
+                  pokemon.imageUrl,
+                  height: 196,
+                  width: 196,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
           ),
-          SizedBox(height: 8),
-          Text(
-            'Weight: ${pokemon.weight}',
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 8),
-          Text(
-            'Height: ${pokemon.height}',
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 8),
-          Text(
-            'Types: ',
-            style: TextStyle(fontSize: 20),
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: pokemon.types.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Text(
-                '- ${pokemon.types[index]}',
-                style: TextStyle(fontSize: 16),
-              );
-            },
-          ),
-          Text(
-            'Stats: ',
-            style: TextStyle(fontSize: 20),
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: pokemon.stats.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Text(
-                '- ${pokemon.stats[index]}',
-                style: TextStyle(fontSize: 16),
-              );
-            },
+          // третья строка
+          Row(
+            children: [
+              // первый столбец
+              Expanded(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Weight: ${pokemon.weight}',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Height: ${pokemon.height}',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
