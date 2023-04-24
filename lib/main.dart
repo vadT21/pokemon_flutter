51e49264181444eceb4f82e9d3ebc82df888cdb1
Vadim <vadimtomin21@yandex.by>
2023-04-24 09:03:30 +0200

    add connectivity
    add connectivity to check status

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:pokemon_flutter/models/pokemon.dart';
 import 'package:pokemon_flutter/screens/pokemon_detail.dart';
 import 'package:pokemon_flutter/screens/pokemon_home.dart';
@@ -9,16 +10,24 @@ void main() async {
   // Инициализируем базу данных
   WidgetsFlutterBinding.ensureInitialized();
   // Загружаем данные из API
-  final apiData = await fetchPokemonList();
-  // Сохраняем данные в базу данных
-  for (final pokemon in apiData) {
-    await DatabaseHelper.insertPokemon(pokemon);
+
+  // Проверка статуса подключения к интернету
+  final connectivityResult = await Connectivity().checkConnectivity();
+  if (connectivityResult == ConnectivityResult.none) {
+    // нет подключения к интернету
+  } else {
+    // есть подключение к интернету
+    final apiData = await fetchPokemonList();
+    // Сохраняем данные в базу данных
+    for (final pokemon in apiData) {
+      await DatabaseHelper.insertPokemon(pokemon);
+    }
   }
   // Запускаем приложение
-  runApp(MyApp());
+  runApp(PokemonApp());
 }
 
-class MyApp extends StatelessWidget {
+class PokemonApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,7 +37,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => HomePage(),
+        '/': (context) => PokemonHomePage(),
         '/pokemon-details': (context) => PokemonDetailScreen(
             pokemon: ModalRoute.of(context)?.settings.arguments as Pokemon),
       },
