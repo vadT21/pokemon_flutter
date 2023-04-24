761cf87efa7f446abd80cbd8c5c7bfce7cc3d1eb
Vadim <vadimtomin21@yandex.by>
2023-04-24 21:54:22 +0200

    change api fetch param
    change api fetch param

--- lib/services/pokemon_api_service.dart
+++ lib/services/pokemon_api_service.dart
@@ -2,9 +2,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:pokemon_flutter/models/pokemon.dart';
 
-Future<List<Pokemon>> fetchPokemonList() async {
-  final response =
-      await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=10'));
+Future<List<Pokemon>> fetchPokemonList(int offset) async {
+  const int limit = 10;
+  final response = await http.get(Uri.parse(
+      'https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset * 10}'));
 
   if (response.statusCode == 200) {
     final jsonBody = jsonDecode(response.body);
